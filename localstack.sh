86e5640a5f95fa545f4b799dff6376185f6c9bb2
Jaime Hernandez <jaime.hernandez@mirai-advisory.com>
2024-04-02 10:39:43 +0200

    feat: Added script to create queue

--- localstack.sh
+++ localstack.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-awslocal s3api create-bucket --bucket mybucket --create-bucket-configuration LocationConstraint=eu-central-1
+# create local bucket
+awslocal s3api create-bucket --bucket mybucket --create-bucket-configuration LocationConstraint=eu-central-1 && awslocal sqs create-queue --queue-name test-queue --region eu-central-1
--- src/main/java/com/jh/awslocalstack/s3/S3Controller.java
+++ src/main/java/com/jh/awslocalstack/s3/S3Controller.java
@@ -5,40 +5,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.services.s3.S3Client;
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
 public class S3Controller {
-
-    private final S3Client s3Client;
-
     private final S3Template s3Template;
 
-    public S3Controller (S3Client s3Client, S3Template s3Template) {
-        this.s3Client = s3Client;
-        this.s3Template = s3Template;
-    }
     @Value("${aws.bucket}")
     private String bucketName;
-    @GetMapping("/buckets")
-    public List<String> getBuckets(){
-        return s3Client.listBuckets()
-                .buckets()
-                .stream()
-                .map(bucket -> bucket.name())
-                .toList();
-    }
 
+    public S3Controller (S3Template s3Template) {
+        this.s3Template = s3Template;
+    }
     @GetMapping("/buckets/template")
     public List<String> getBucketsWithTemplate(){
         return s3Template.listObjects(bucketName, "").stream()
                 .map(s3Resource -> s3Resource.getFilename())
                 .toList();
     }
-
     @PostMapping("/buckets")
     public ResponseEntity<String> uploadFile(@RequestParam MultipartFile file) throws IOException {
         s3Template.upload(bucketName, file.getOriginalFilename(), file.getInputStream());
